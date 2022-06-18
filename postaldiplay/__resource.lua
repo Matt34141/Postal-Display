8bca721c79eb773f0dccdb139ce5c0c61a1e0913
Matt <107737850+Matt34141@users.noreply.github.com>
2022-06-17 23:48:59 -0300

    Add files via upload

---
+++ postaldiplay/__resource.lua
@@ -0,0 +1,11 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+files {
+	'Newtonsoft.Json.dll',
+	'postals.json'
+}
+
+client_scripts {
+	'Common.Client.net.dll',
+	'PostalDisplay.Client.net.dll',
+}
